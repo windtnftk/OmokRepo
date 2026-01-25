f697a271bc064a552a177b87c9e9a260735dd9cd
chanhwi Han <windtnftk@naver.com>
2026-01-25 11:31:42 +0900

    Client : class Libarty.dll add

---
+++ shared/ClassLibrary1/PacketEvent.cs
@@ -0,0 +1,19 @@
+﻿using Protocol;
+using System;
+using System.Net.Sockets;
+
+namespace NetworkClientApp
+{
+    public class PacketEvent
+    {
+        public PacketType Type { get; }
+        public byte[] Payload { get; }
+
+        // 역할: 패킷 이벤트 객체를 초기화한다.
+        public PacketEvent(PacketType type, byte[] payload)
+        {
+            Type = type;
+            Payload = payload ?? Array.Empty<byte>();
+        }
+    }
+}
